40b3db848717c968fc3d98408270c5be2aa8185d
Romulo-Viana <romulovianalast@gmail.com>
2024-01-14 13:23:25 -0300

    exe 20 e 21 criados

---
+++ aula12/exe020.js
@@ -0,0 +1,9 @@
+idade = 18
+
+if (idade < 16){
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65){
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatorio')
+}
