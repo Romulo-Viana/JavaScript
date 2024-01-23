dedaea77f0de8447ec01f4a507cebe74533f76a9
Romulo-Viana <romulovianalast@gmail.com>
2024-01-23 08:04:18 -0300

    aula 13, exe025 em andamento...

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+
+//Contador usando teste lógico no final
+c = 1
+do {
+    console.log(c)
+    c += 1
+}while (c <= 6)
+
+
+
+
+
+
+
+
+
+//Contador usando teste lógico no início
+c = 1
+while(6 >= c){
+    console.log(c)
+    c +=1
+}
+
