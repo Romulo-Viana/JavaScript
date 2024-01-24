bfa21b4f62ee03325bdd7d305a7151e7d1fdc56e
Romulo-Viana <romulovianalast@gmail.com>
2024-01-24 08:23:28 -0300

    Aula 14 add

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+
+for(c=1/*1ª inicia*/ ;c<=5/*2ª testa*/ ;c+=1/*4ª incrementa*/){
+    console.log(c)//3ª exeuta o bloco
+}
+
+
+/*c=1 //1ª inicia
+while(c<=5){//2ª testa
+    console.log(c)//3ª Exucuta o bloco
+    c++ //4ª incrementa
+}
+*/
